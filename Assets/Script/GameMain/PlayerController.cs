7823ac968037692c93bc676752f0984c1522bc0c
朝日 大貴 <asahi.ruit@gmail.com>
2026-08-04 23:25:27 +0900

    操作方法変更

--- Assets/Script/GameMain/PlayerController.cs
+++ Assets/Script/GameMain/PlayerController.cs
@@ -61,8 +61,7 @@ public class PlayerController : MonoBehaviour
         if (Gamepad.current != null)
         {
             Vector2 stickInput = Gamepad.current.leftStick.ReadValue();
-            kbRoll = stickInput.x;   // 左右でロール
-            // 簡易版
+            // kbRoll = stickInput.x;   // 左右でロール
             kbYaw = stickInput.x;   // 左右でヨー(共通化することで操作が簡易になる)
             kbPitch = stickInput.y;  // 上下でピッチ
         }
